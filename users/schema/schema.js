70a3fd57dff330b1025637c184ae6e9022a9e9fa
WINDOWS-IFPK7JI\Elijah <code.elijahchen@gmail.com>
2017-06-13 18:14:56 -0400

    - Added company to rootquery

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -37,12 +37,20 @@ const RootQuery = new GraphQLObjectType({
     fields: {
        user: {
            type: UserType,
-           args: { id: { type: GraphQLString }},
+           args: { id: { type: GraphQLString } },
            resolve(parentValue, args) {
                return axios.get(`http://localhost:3000/users/${args.id}`)
                    .then(res => res.data);
            }
-       }
+       },
+        company: {
+           type: CompanyType,
+            args: { id: {type: GraphQLString } },
+            resolve(parentValue, args){
+               return  axios.get(`http://localhost:3000/companies/${args.id}`)
+                .then(res => res.data);
+            }
+        }
     }
 });
 

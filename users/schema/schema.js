3aa7cb8071551005ad69216398c527bac8a7163b
WINDOWS-IFPK7JI\Elijah <code.elijahchen@gmail.com>
2017-06-13 20:43:52 -0400

    - Added company to rootquery, CompanyType schema can return a list of users associated with it, added mandatory wrapper/closure to schema field

--- users/schema/schema.js
+++ users/schema/schema.js
@@ -8,17 +8,24 @@ const {
 } = graphql;
 
 const CompanyType = new GraphQLObjectType ({
-   name: 'Company',
-    fields: {
+    name: 'Company',
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
-        description: { type: GraphQLString }
-    }
+        description: { type: GraphQLString },
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args){
+                return axios.get(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then(res => res.data);
+            }
+        }
+    })
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString},
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
@@ -29,7 +36,7 @@ const UserType = new GraphQLObjectType({
                     .then(res => res.data);
             }
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
